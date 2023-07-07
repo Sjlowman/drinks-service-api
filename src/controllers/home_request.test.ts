bfbea69cd2f77ecf6daa3c630eaedb3fe4155745
Sjlowman <sarah.j.lowman@gmail.com>
2023-07-07 09:55:19 +0100

    feat: Implement GET request for coffeelover endpoint and test

--- src/controllers/home_request.test.ts
+++ src/controllers/home_request.test.ts
@@ -2,9 +2,16 @@ import request from "supertest";
 import { app } from "../app";
 
 describe("Test home API endpoint request", () => {
-  test("GET should return correct message", async () => {
+  test("Homepoint GET should return correct message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual("Welcome to the Drinks API!");
+  });
+});
+describe("Test coffeelover endpoint request", () => {
+  test("coffeelover GET should return correct message", async () => {
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
   });
 });
